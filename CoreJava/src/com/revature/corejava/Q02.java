1210d80f9fc369e0f91e5c0a971de89492fa3508
Cody Johnson <codyj@protonmail.com>
2019-02-12 18:12:19 -0500

    Add Q02 solution

---
+++ CoreJava/src/com/revature/corejava/Q02.java
@@ -0,0 +1,50 @@
+package com.revature.corejava;
+
+public class Q02 {
+	private int n;
+	
+	// Default constructor
+	Q02() {
+		this.n = 25;
+	}
+	
+	// Constructor to assign custom Fib index
+	Q02(int idx) {
+		this.n = idx;
+	}
+	
+	// List Fibonacci numbers up to n
+	public void fib() {
+		int[] list = new int[this.n];
+		
+		// Compute and store Fibonacci numbers
+		for (int i=0; i<this.n; i++) {
+			if (i==0) {
+				list[i] = 0;
+			}
+			else if (i==1) {
+				list[i] = 1;
+			}
+			else {
+				list[i] = list[i-2] + list[i-1];
+			}
+		}
+		
+		// Output Fibonacci numbers
+		for (int i:list) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+	
+	// Main
+	public static void main(String[] args) {
+		// Default
+		Q02 q0 = new Q02();
+		q0.fib();
+		
+		// Custom
+		Q02 q1 = new Q02(10);
+		q1.fib();
+	}
+}
