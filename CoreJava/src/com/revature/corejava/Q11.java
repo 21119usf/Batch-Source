898647d0983fced2320b048de266e1c9e032e6b5
Cody Johnson <codyj@protonmail.com>
2019-02-13 18:40:40 -0500

    Add Q11 solution

---
+++ CoreJava/src/com/revature/corejava/Q11.java
@@ -0,0 +1,12 @@
+package com.revature.corejava;
+
+import com.revature.corejava.q11.package01.*;
+
+// Access floats in another package
+public class Q11 {
+	// Main
+	public static void main(String[] args) {
+		Q11Floats floats = new Q11Floats();
+		System.out.println(floats.a + " " + floats.b);
+	}
+}
---
+++ CoreJava/src/com/revature/corejava/q11/package01/Q11Floats.java
@@ -0,0 +1,6 @@
+package com.revature.corejava.q11.package01;
+
+public class Q11Floats {
+	public float a = 33.3f;
+	public float b = 13.0f;
+}
