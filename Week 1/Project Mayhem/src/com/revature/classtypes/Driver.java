7b830531b3e87f0f7f0a906d0aac7875c9646399
xD12 <george_45a@hotmail.com>
2019-02-13 19:17:11 -0500

    new adds

---
+++ Week 1/Project Mayhem/src/com/revature/classtypes/Driver.java
@@ -0,0 +1,15 @@
+package com.revature.classtypes;
+
+import com.revature.beans.Person;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Shark shark = new Shark();
+		shark.breathe();
+		shark.findPrey();
+		Person p= new Person();
+		p.findPrey();
+	}
+
+}
