b0725d9b88ee57fd01e437171fba8738d224bdd4
Cody Johnson <codyj@protonmail.com>
2019-02-14 11:27:47 -0500

    Update Q15 solution

--- CoreJava/src/com/revature/corejava/Q15.java
+++ CoreJava/src/com/revature/corejava/Q15.java
@@ -5,7 +5,7 @@ public class Q15 implements Q15Interface {
 	public int addition(int a, int b) {
 		return a+b;
 	}
-	public int substraction(int a, int b) {
+	public int subtraction(int a, int b) {
 		return a-b;
 	}
 	public int multiplication(int a, int b) {
@@ -18,7 +18,9 @@ public class Q15 implements Q15Interface {
 	// Main
 	public static void main(String[] args) {
 		Q15 q0 = new Q15();
-		int sum = q0.addition(10, 11);
-		System.out.println(sum);
+		System.out.println(q0.addition(10, 11));
+		System.out.println(q0.subtraction(10, 11));
+		System.out.println(q0.multiplication(10, 11));
+		System.out.println(q0.division(10, 11));
 	}
 }
--- CoreJava/src/com/revature/corejava/Q15Interface.java
+++ CoreJava/src/com/revature/corejava/Q15Interface.java
@@ -2,7 +2,7 @@ package com.revature.corejava;
 
 public interface Q15Interface {
 	public int addition(int a, int b);
-	public int substraction(int a, int b);
+	public int subtraction(int a, int b);
 	public int multiplication(int a, int b);
 	public int division(int a, int b);
 }
