265dcf01f97e1a3a702ef08ba0e3d61d53287488
Cody Johnson <codyj@protonmail.com>
2019-02-15 21:06:20 -0500

    Update Admin, Employee

---
+++ BankApp/src/main/java/com/bankapp/person/Admin.java
@@ -0,0 +1,9 @@
+package com.bankapp.person;
+
+// Admin class
+public class Admin extends Employee {
+	// Constructor
+	Admin(String un, String pw) {
+		super(un, pw);
+	}
+}
---
+++ BankApp/src/main/java/com/bankapp/person/Employee.java
@@ -0,0 +1,55 @@
+package com.bankapp.person;
+
+import com.bankapp.account.Account;
+
+// Employee class
+public class Employee extends Person {
+	private static int count = 0;
+	private int id;
+	private String username;
+	private String password;
+	
+	// Constructor
+	Employee(String un, String pw) {
+		setId(count++);
+		setUsername(un);
+		setPassword(pw);
+	}
+	
+	// Approve an account
+	public void approveAccount(Account a) {
+		a.setOpen(true);
+	}
+	// Deny an account
+	public void denyAccount(Account a) {
+		a.setOpen(false);
+	}
+	// Retrieve customer information
+	//public Customer getCustomerInfo(int id) {
+		// Find customer by id
+	//}
+	// Retrieve account information
+	//public Account getAccountInfo(int id) {
+		// find account by id
+	//}
+
+	// Get/set methods
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
