aaf6afe0be837d532afb4d232fc1437d81998866
Cody Johnson <codyj@protonmail.com>
2019-02-15 20:38:24 -0500

    Update Account

--- BankApp/src/main/java/com/bankapp/account/Account.java
+++ BankApp/src/main/java/com/bankapp/account/Account.java
@@ -9,6 +9,7 @@ import com.bankapp.person.Customer;
 public class Account {
 	private UUID id;
 	private String status;
+	private boolean open;
 	private double balance;
 	private ArrayList<Customer> owners;
 	
@@ -16,8 +17,51 @@ public class Account {
 	Account() {
 		setId(UUID.randomUUID());
 		setStatus("Pending");
+		setOpen(false);
 		setBalance(0.00);
 		setOwners(new ArrayList<Customer>());
+	}
+	
+	// Deposit into account
+	public void deposit(double d) {
+		if (isOpen()) {
+			balance += d;
+		} else {
+			System.out.println("DENIED. Account status: " + status);
+		}
+	}
+	// Withdraw from account
+	public void withdraw(double d) {
+		if (isOpen()) {
+			if (!willOverdraw(d)) {
+				balance -= d;
+			}
+		} else {
+			System.out.println("DENIED. Account status: " + status);
+		}
+	}
+	// Transfer funds from this account into another
+	public void transfer(Account a, double d) {
+		if (isOpen()) {
+			if (!willOverdraw(d)) {
+				a.deposit(d);
+				balance -= d;
+			}
+		} else {
+			System.out.println("DENIED. Account status: " + status);
+		}
+	}
+	// Check for overdraw
+	public boolean willOverdraw(double d) {
+		if (d > balance) {
+			System.out.println("DENIED:  Not enough funds");
+			return true;
+		}
+		return false;
+	}
+	// Add owner to account
+	public void addOwner(Customer c) {
+		owners.add(c);
 	}
 
 	// Get/set methods
@@ -44,5 +88,11 @@ public class Account {
 	}
 	public void setOwners(ArrayList<Customer> owners) {
 		this.owners = owners;
+	}
+	public boolean isOpen() {
+		return open;
+	}
+	public void setOpen(boolean open) {
+		this.open = open;
 	}
 }
