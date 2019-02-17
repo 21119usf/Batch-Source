85841899b59582efa54a7e0a2b51c21a7c6b9e2c
Cody Johnson <codyj@protonmail.com>
2019-02-17 16:40:44 -0500

    Update Admin

--- BankApp/src/main/java/com/bankapp/person/Admin.java
+++ BankApp/src/main/java/com/bankapp/person/Admin.java
@@ -5,7 +5,7 @@ public class Admin extends Employee {
 	private static final long serialVersionUID = -1258495181731654754L;
 
 	// Constructor
-	Admin(String un, String pw) {
-		super(un, pw);
+	Admin(String un, String pw, String fn, String ln) {
+		super(un, pw, fn, ln);
 	}
 }
