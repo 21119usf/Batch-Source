cbd89ce231afc2289f79ba6dbb8c78fb28939aa1
Cody Johnson <codyj@protonmail.com>
2019-02-17 16:01:54 -0500

    Update Menu

--- BankApp/src/main/java/com/bankapp/menu/Menu.java
+++ BankApp/src/main/java/com/bankapp/menu/Menu.java
@@ -1,7 +1,6 @@
 package com.bankapp.menu;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
@@ -17,10 +16,10 @@ public class Menu {
 	public int display() {
 		int option = 0;
 		boolean notValidOption = true;
-		
-		int count = 0;
+
 		do {
 			// Display menu
+			int count = 0;
 			for (String item : items) {
 				System.out.println(count + ". " + item);
 				count++;
@@ -28,17 +27,17 @@ public class Menu {
 			System.out.print(">>> ");
 			
 			// Get input
-			try {
+			if (sc.hasNextInt()) {
 				option = sc.nextInt();
-			} catch (InputMismatchException e) {
-				System.out.println("Invalid option. Try again.");
-				sc.nextLine();
-			}
-			if (option >=0 && option < items.size()) {
+				sc.nextLine();    // Clear buffer
+				if (option < 0 || option > items.size()) {
+					System.out.println("Invalid option");
+					continue;
+				}
 				notValidOption = false;
-				sc.nextLine();	// Clear buffer
 			} else {
-				System.out.println("Invalid option. Try again.");
+				System.out.println("Invalid option");
+				sc.nextLine();
 			}
 		} while (notValidOption);
 		
