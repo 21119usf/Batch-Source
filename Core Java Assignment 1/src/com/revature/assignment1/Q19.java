8ec9d069a5037fa08c9bae815570e7502c86a8f4
Joe Judan <joe.judan@gmail.com>
2019-02-18 11:05:23 -0500

    Add Assignment 1

--- Core Java Assignment 1/src/com/revature/assignment1/Q19.java
+++ Core Java Assignment 1/src/com/revature/assignment1/Q19.java
@@ -7,35 +7,35 @@ public class Q19 {
 	public static void main(String[] args) {
 		ArrayList<Integer> intList  = new ArrayList<Integer>();//arrayList
 		
-		for(int i = 1;i<11;i++) {//method to insert numbers 1 -  10 into arraylist
+		for(int i = 1;i<11;i++) {//loop to insert numbers 1 -  10 into arraylist
 			intList.add(i);//insert number
 		}
 		int j=0;
-		for(int i = 0;i<10;i+=2) {//method to get even numbers
+		for(int i = 0;i<10;i+=2) {//loop to get even numbers
 			j=j+intList.get(i);//adding sum of all even numbers
-			System.out.println("Adding "+intList.get(i));//display result
 		}
-		System.out.println(j);
+		System.out.println("Sum of even is "+j);
 		int k=0;
 		for(int i = 1;i<10;i+=2) {//method to get odd numbers
 			k=k+intList.get(i);//adding sum of all odd numbers
-			System.out.println("Adding "+intList.get(i));//display result
 		}
-		System.out.println(k);
-		int m = 1;
-		for(int i=2;i<intList.size();i++) {
-			for(int l = 2;l<=i;l++){
-				if(l==i){
-					System.out.println("Dropping "+i+" because its prime.");
+		System.out.println("Sum of odd is "+k);
+		int sum=0;
+		boolean primeNum=true;
+		
+		for(int m=2;m<intList.size();m++) {//for loop for iterator to check each  number
+			
+			for(int i=2;i<=m/2;i++) {//for loop to check for prime
+
+				if(m%i==0) {
+					//not prime
+					primeNum=false;
 					break;
-				}
-				if(i%l==0){
-					
-					System.out.println("Adding "+ i);
-					m+=i;
-				}
+				}else
+					sum=sum+m;
 			}
+
 		}
-		System.out.println(m);
+		System.out.println("Sum of prime is "+sum);//display result
 	}
 }
