842b2abce709eac4dc1ba58cc0e2d80eb968ee21
cnitsch821 <38124147+cnitsch821@users.noreply.github.com>
2019-02-18 11:03:01 -0500

    Add files via upload

--- src/CoreJava/Homework1/Q13.java
+++ src/CoreJava/Homework1/Q13.java
@@ -2,15 +2,21 @@ package com.revature.homework;
 
 public class Q13 {
 	
-	public static void main(String[] args) {
-		
-		for (int i=1; i<5; i += 1)
-		{
-		    for (int j=0; j<i; j++)
-		    {
-		    	System.out.print(((j+i)%2)+" ");
-		    }
-		    System.out.println("");
-		}
-	}
-}
+    public static boolean sqrt(int x) { //prints rows
+        double a = Math.sqrt(8*x + 1);
+        return (a - Math.floor(a) == 0);
+    }
+    
+   public static void printArray(int[] runArray) { //run array
+       for (int i=1; i<=runArray.length; i++) {
+           System.out.print(runArray[i-1] + " ");
+           if (sqrt(i)) {
+               System.out.println();
+           }
+       }
+   }
+       public static void main(String[] args) { //print array
+          int[] array1 = {0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
+          printArray(array1);
+       }
+    }
