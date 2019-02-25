ca84ab4403d2d82d2f916b18f0ad87898aefdd8c
MarkVenegas <mvenegas6275@gmail.com>
2019-02-24 23:06:33 -0500

    Finished my sql homework.

--- MVengas_SQLlab.sql
+++ MVengas_SQLlab.sql
@@ -95,7 +95,7 @@ ROLLBACK TO BEFORE_DELETE;
 --3.1 System Defined Functions
 --Task – Create a function that returns the current time.
 CREATE OR REPLACE FUNCTION RETURN_CURR_TIME
-RETURN DATE AS
+RETURN DATE AS              -- IF NO ALIAS FOR THE RETURN TYPE DONT PUT A ';'
 BEGIN
 RETURN CURRENT_TIMESTAMP;
 END;
@@ -130,12 +130,255 @@ END;
 SELECT AVERAGE_INVOICES FROM DUAL;
 
 --Task – Create a function that returns the most expensive track
+CREATE OR REPLACE FUNCTION MOST_EXPENSIVE
+RETURN SYS_REFCURSOR AS
+EXP_TRACKS SYS_REFCURSOR;
+BEGIN
+OPEN EXP_TRACKS FOR
+SELECT * FROM TRACK
+WHERE UNITPRICE = (SELECT MAX(UNITPRICE) FROM TRACK);
+RETURN EXP_TRACKS;
+END;
+/
+--HAD TO RETURN A CURSOR SINCE THERE IS MORE THAN ONE TRACK WITH THE MOST EXPENSIVE PRICE AT 1.99
+SELECT MOST_EXPENSIVE FROM DUAL;
 
 
 --3.3 User Defined Scalar Functions
 --Task – Create a function that returns the average price of invoiceline items in the invoiceline table
+CREATE OR REPLACE FUNCTION AVG_PRICE
+RETURN INVOICELINE.UNITPRICE%TYPE AS
+AVERAGE INVOICELINE.UNITPRICE%TYPE;
+BEGIN
+SELECT AVG(UNITPRICE) INTO AVERAGE
+FROM INVOICELINE;
+RETURN AVERAGE;
+END;
+/
+
+SELECT AVG_PRICE FROM DUAL;
+
 --3.4 User Defined Table Valued Functions
 --Task – Create a function that returns all employees who are born after 1968.
+CREATE OR REPLACE FUNCTION BORN_AFTER
+RETURN SYS_REFCURSOR AS
+AFTER_DATE SYS_REFCURSOR;
+BEGIN
+OPEN AFTER_DATE FOR 
+SELECT * FROM EMPLOYEE
+WHERE BIRTHDATE >= '01-JAN-1968';
+RETURN AFTER_DATE;
+END;
+/
+
+SELECT BORN_AFTER FROM DUAL;
+
+
+--4.0 Stored Procedures
+-- In this section you will be creating and executing stored procedures.
+--You will be creating various types of stored procedures that take input and output parameters.
+--4.1 Basic Stored Procedure
+--Task – Create a stored procedure that selects the first and last names of all the employees.
+CREATE OR REPLACE PROCEDURE FIRSTLASTNAMES
+AS RC  SYS_REFCURSOR;
+BEGIN
+OPEN RC FOR
+SELECT FIRSTNAME, LASTNAME FROM EMPLOYEE;
+dbms_sql.return_result(RC);
+END;
+/
+
+EXECUTE FIRSTLASTNAMES;
+
+--4.2 Stored Procedure Input Parameters
+--Task – Create a stored procedure that updates the personal information of an employee.
+CREATE OR REPLACE PROCEDURE UPDATE_EMPLOYEE
+(OLD_TITLE EMPLOYEE.TITLE%TYPE, NEW_TITLE EMPLOYEE.TITLE%TYPE)
+AS
+BEGIN
+UPDATE EMPLOYEE
+SET TITLE = NEW_TITLE
+WHERE TITLE = OLD_TITLE;
+END;
+/
+
+EXECUTE UPDATE_EMPLOYEE('IT Manager', 'Trainer');
+
+
+--Task – Create a stored procedure that returns the managers of an employee .
+CREATE OR REPLACE PROCEDURE RET_MANAGERS
+(EMP_FIRST VARCHAR2, EMP_LAST VARCHAR2)
+AS RC SYS_REFCURSOR;
+BEGIN
+OPEN RC FOR
+SELECT FIRSTNAME, LASTNAME, TITLE FROM EMPLOYEE
+WHERE EMPLOYEEID = (SELECT REPORTSTO FROM EMPLOYEE WHERE FIRSTNAME = EMP_FIRST AND LASTNAME = EMP_LAST);
+dbms_sql.return_result(RC);
+END;
+/
+
+EXECUTE RET_MANAGERS('Robert', 'King');
+
+
+--4.3 Stored Procedure Output Parameters
+--Task – Create a stored procedure that returns the name and company of a customer.
+CREATE OR REPLACE PROCEDURE NAMECOMPANY
+(CUST NUMBER)
+AS RC SYS_REFCURSOR;
+BEGIN
+OPEN RC FOR
+SELECT FIRSTNAME, LASTNAME, COMPANY FROM CUSTOMER
+WHERE CUSTOMERID = CUST;
+dbms_sql.return_result(RC);
+END;
+/
+
+EXECUTE NAMECOMPANY(10);
+
+
+--5.0 Transactions
+--In this section you will be working with transactions. Transactions are usually nested within a stored procedure.
+--Task – Create a transaction that given a invoiceId will delete that invoice
+-- (There may be constraints that rely on this, find out how to resolve them).
+CREATE OR REPLACE PROCEDURE DELETE_INVOICE
+(INVOICE_NUM NUMBER)
+AS
+BEGIN
+DELETE FROM INVOICELINE
+WHERE INVOICEID = INVOICE_NUM;
+DELETE FROM INVOICE
+WHERE INVOICEID = INVOICE_NUM;
+COMMIT;
+END;
+/
+
+EXECUTE DELETE_INVOICE(113);
+--SELECT COUNT(*) FROM INVOICELINE
+--WHERE INVOICEID = 113;
+
+--Task – Create a transaction nested within a stored procedure that inserts a new record in the Customer table
+CREATE OR REPLACE PROCEDURE INSERT_CUSTOMER
+(NUMID NUMBER, FNAME VARCHAR2, LNAME VARCHAR2, EMAIL VARCHAR2)
+AS
+BEGIN
+INSERT INTO CUSTOMER VALUES(NUMID, LNAME, FNAME, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, EMAIL, NULL);
+COMMIT;
+END;
+/
+
+EXECUTE INSERT_CUSTOMER(1001, 'Mark', 'Venegas', 'markavenegas@hotmail.com');
+
+
+--6.0 Triggers
+--In this section you will create various kinds of triggers that work when certain DML statements are executed on a table.
+--6.1 AFTER/FOR
+--Task - Create an after insert trigger on the employee table fired after a new record is inserted into the table.
+CREATE SEQUENCE MYSEQ
+MINVALUE 100
+MAXVALUE 10000
+START WITH 5000
+INCREMENT BY -5
+CACHE 20;
+
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE TRIGGER INSERT_EMP
+AFTER INSERT ON EMPLOYEE
+DECLARE
+NUM_EMPS NUMBER;
+BEGIN
+SELECT COUNT(*) INTO NUM_EMPS FROM EMPLOYEE;
+DBMS_OUTPUT.PUT_LINE('NUMBER OF EMPLOYEES: ' || NUM_EMPS);
+END;    
+/
+
+DROP TRIGGER INSERT_EMP;
+
+INSERT INTO EMPLOYEE VALUES(1001, 'Venegas', 'Erik', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+DELETE FROM EMPLOYEE
+WHERE FIRSTNAME = 'Erik';
+
+
+--Task – Create an after update trigger on the album table that fires after a row is inserted in the table
+CREATE OR REPLACE TRIGGER UPDATE_ALBUM
+AFTER UPDATE ON ALBUM
+FOR EACH ROW
+BEGIN
+DBMS_OUTPUT.PUT_LINE('OLD TITLE: ' || :OLD.TITLE);
+DBMS_OUTPUT.PUT_LINE('NEW TITLE: ' || :NEW.TITLE);
+END;
+/
+
+DROP TRIGGER UPDATE_ALBUM;
+
+UPDATE ALBUM
+SET TITLE = 'Lifelight'
+WHERE TITLE = 'War';
+
+UPDATE ALBUM
+SET TITLE = 'War'
+WHERE TITLE = 'Lifelight';
+
+
+--Task – Create an after delete trigger on the customer table that fires after a row is deleted from the table.
+CREATE OR REPLACE TRIGGER DELETE_CUST
+AFTER DELETE ON CUSTOMER
+DECLARE
+NUM_CUSTS NUMBER;
+BEGIN
+SELECT COUNT(*) INTO NUM_CUSTS FROM CUSTOMER;
+DBMS_OUTPUT.PUT_LINE('NUMBER OF CUSTOMERS: ' || NUM_CUSTS);
+END;    
+/
+
+DROP TRIGGER DELETE_CUST;
+
+INSERT INTO CUSTOMER VALUES(1000, 'Mark', 'Venegas', 'Revature', '8327 Serenity Ct.', 'El Cajon', 'CA', 'United States', '92021', '+1 (619) 831-2031', NULL, 'markavenegas@hotmail.com', NULL);
+
+DELETE FROM CUSTOMER
+WHERE FIRSTNAME = 'Mark' AND LASTNAME = 'Venegas';
+
+
+--7.0 JOINS
+--In this section you will be working with combining various tables through the use of joins. You will work with outer, inner, right, left, cross, and self joins.
+--7.1 INNER
+--Task – Create an inner join that joins customers and orders and specifies the name of the customer and the invoiceId.
+SELECT CUSTOMER.CUSTOMERID, CUSTOMER.FIRSTNAME, CUSTOMER.LASTNAME, INVOICE.INVOICEID
+FROM CUSTOMER
+INNER JOIN INVOICE
+ON CUSTOMER.CUSTOMERID = INVOICE.CUSTOMERID
+ORDER BY CUSTOMERID ASC;
+
+--7.2 OUTER
+--Task – Create an outer join that joins the customer and invoice table, specifying the CustomerId, firstname, lastname, invoiceId, and total.
+SELECT CUSTOMER.CUSTOMERID, CUSTOMER.FIRSTNAME, CUSTOMER.LASTNAME, INVOICE.INVOICEID, INVOICE.TOTAL
+FROM CUSTOMER, INVOICE
+WHERE CUSTOMER.CUSTOMERID = INVOICE.CUSTOMERID(+);
+
+--7.3 RIGHT
+--Task – Create a right join that joins album and artist specifying artist name and title.
+SELECT ARTIST.ARTISTID, ARTIST.NAME, ALBUM.TITLE
+FROM ARTIST
+RIGHT JOIN ALBUM
+ON ARTIST.ARTISTID = ALBUM.ARTISTID
+ORDER BY ARTISTID;
+
+--7.4 CROSS
+--Task – Create a cross join that joins album and artist and sorts by artist name in ascending order.
+SELECT * FROM ALBUM
+CROSS JOIN ARTIST
+ORDER BY ARTIST.NAME ASC;
+
+--7.5 SELF
+--Task – Perform a self-join on the employee table, joining on the reportsto column.
+SELECT * FROM EMPLOYEE A, EMPLOYEE B
+WHERE A.REPORTSTO = B.EMPLOYEEID;
+
+
+
+
+
 
 
 
