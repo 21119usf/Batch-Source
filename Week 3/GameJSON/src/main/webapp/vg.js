6667e692996833bc855321dec6d110ef34e5f5a3
Jason Knighten <jmknighten9@gmail.com>
2019-03-16 17:37:05 -0400

    added updates to html and js on GamsJSON

--- Week 3/GameJSON/src/main/webapp/vg.js
+++ Week 3/GameJSON/src/main/webapp/vg.js
@@ -1,4 +1,8 @@
-
+window.onload=function(){
+    console.log("in onload");
+    document.getElementById("vgSubmitGet").addEventListener("click",getVG,false);
+    //document.getElementById("vgpost").addEventListener("click",postVG,false);
+}
 
 function loadVG(vg){
     document.getElementById("vgName").innerHTML=vg.vgName;
@@ -6,16 +10,18 @@ function loadVG(vg){
 
 function getVG(){
     console.log("in getVG");
-    let vgID= document.getElementById("vgID").Value;
-    let xhr =new XMLHttpRequest();
+    var vgid= document.getElementById("vgID").value;
+    var xhr =new XMLHttpRequest();
     xhr.onreadystatechange=function(){
-        console.log("in ORSC");
+        console.log("in ORSC "+xhr.readyState);
         if(xhr.readyState==4 && xhr.status==200){
             console.log(xhr.responseText);
             var vg= JSON.parse(xhr.responseText);
             loadVG(vg);
+        }
     }
-    xhr.open("GET","http://localhost:8080/GameJSON/vg?vgid="+vgID,true);
+  
+    xhr.open("GET","http://localhost:8080/GameJSON/vg?vgid="+vgid,true);
     xhr.send();
 }
 
@@ -29,8 +35,3 @@ function postVG(){
 
 
 
-window.onload=function(){
-    console.log("in onload");
-    document.getElementById("vgsubmitGet").addEventListener("click",getVG,false);
-    document.getElementById("vgpost").addEventListener("click",postVG,false);
-}
